950e441beb40f29359f4817e17bc0107bd441382
Cliff Rodgers <cliff@promptworks.com>
2017-02-10 16:23:00 -0500

    WIP toward parsing URL

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [hickory "0.7.0"]]
   :profiles {:uberjar {:aot :all}
              :dev {:plugins [[lein-binplus "0.6.2"]]}}
   :bin {:name "weather"}
--- src/weather/core.clj
+++ src/weather/core.clj
@@ -1,5 +1,25 @@
 (ns weather.core
+  (:require [clojure.zip :as zip]
+            [hickory.core :as hickory]
+            [hickory.select :as s]
+            [hickory.zip :as hz])
   (:gen-class))
 
+(def weather-url "https://www.wunderground.com/DisplayDisc.asp?DiscussionCode=PHI&StateCode=PA&SafeCityName=Philadelphia")
+
+(def selector #(s/select (s/class "column")
+                         %))
+(comment
+
+  (-> (slurp weather-url)
+      hickory/parse
+      hickory/as-hickory
+      selector)
+  )
+
+
 (defn -main [& args]
-  (println "Gimme weather"))
+  (-> (slurp weather-url)
+      hickory/parse
+      selector
+      println))
